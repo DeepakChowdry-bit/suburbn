f1d1d6d11038db049b652a220b466df6d81b14a5
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2024-08-08 00:18:17 +0530

    page removed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,13 +12,13 @@ const popp = Poppins({
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center h-[75vh] relative">
-        {/* <Image src={'/Img/banner.png'} alt="banner" width={1000} height={1000} className="absolute md:w-11/12 h-full object-cover object-center w-fit px-3"/>
+      {/* <div className="flex justify-center h-[75vh] relative">
+        <Image src={'/Img/banner.png'} alt="banner" width={1000} height={1000} className="absolute md:w-11/12 h-full object-cover object-center w-fit px-3"/>
         <div className="text-white z-20 h-full flex flex-col items-center justify-center text-center space-y-3">
           <h3 className={`font-bold tracking-wide text-4xl ${popp.className}`}>URB<span className="italic">N</span>. <br /><span className="font-medium text-3xl">COLLECTION</span></h3>
           <Link href={'/store'} className="hover:border-b w-fit">Browse Collection</Link>
-        </div> */}
-      </div>
+        </div>
+      </div> */}
       <Subscribe />
       <ProductsGrid />
     </>
