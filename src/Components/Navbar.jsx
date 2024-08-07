8bdab4bfe4e4c207d68bdd44d7d632a81fda6f96
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2024-08-08 00:13:02 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,10 +20,8 @@ const Navbar = () => {
             <div className='flex items-center justify-center h-20 shadow-sm top-0 bg-zinc-600 z-20 border-b border-black w-full'>
                 <div className='flex items-center justify-between w-11/12'>
                     <MenuBar />
-                    <div id="logo" className='flex justify-center'>
-                        <Image src={'/Img/logo-black.svg'} width={100} height={100} alt='' />
-                    </div>
-                    <div className='flex items-center justify-end space-x-5'>
+                    <Image src={'/Img/logo-black.svg'} width={100} height={100} alt='' />
+                    <div className='flex items-center space-x-5'>
                         <SearchBox />
                         <Link href={'/cart'}>
                             <Image id="cart-icon" src={'/Img/bag.svg'} width={30} height={30} alt='bag' />
